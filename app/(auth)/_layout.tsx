d0afc087ed61695c24b91c9d62c54bb30e6aeba3
DEEPESH-845 <dkakkar_be23@thapar.edu>
2025-11-24 18:54:04 +0530

    feat: Integrate appwrite and Auth

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+	Dimensions,
+	Image,
+	ImageBackground,
+	KeyboardAvoidingView,
+	Platform,
+	ScrollView,
+	View,
+} from "react-native";
+
+import { Slot } from "expo-router";
+
+import { images } from "@/constants";
+import CustomButton from "@/components/CustomButton";
+import CustomInput from "@/components/CustomInput";
+
+const _layout = () => {
+	return (
+		<KeyboardAvoidingView
+			behavior={Platform.OS === "ios" ? "padding" : "height"}
+		>
+			<ScrollView
+				className="bg-white h-full"
+				keyboardShouldPersistTaps="handled"
+			>
+				<View
+					className="w-full relative"
+					style={{ height: Dimensions.get("screen").height / 2.25 }}
+				>
+					<ImageBackground
+						source={images.loginGraphic}
+						className="size-full rounded-b-lg"
+						resizeMode="stretch"
+					/>
+					<Image
+						source={images.logo}
+						className="self-center size-48 absolute -bottom-16 z-10"
+					/>
+				</View>
+
+				<CustomButton />
+				<CustomInput />
+			</ScrollView>
+			<Slot />
+		</KeyboardAvoidingView>
+	);
+};
+export default _layout;
---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+platform:"com.dk.nutridesk",
+databaseId: "6924520100130664cbb3",
+userCollectionId: "user"
+}
